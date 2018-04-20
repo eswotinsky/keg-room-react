2961639f36736123313a7aa7b9f066343cf53fd3
Eric Swotinsky <eric.swotinsky@gmail.com>
2018-04-20 08:35:18 -0700

    initialize project.

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+function App(){
+  return (
+    <div>
+      <h1>React project works!</h1>
+      <Switch>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
